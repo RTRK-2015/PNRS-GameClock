046a69bf4f0d2f6a8e3acb03e7f2e351ac338c27
authext <wolf.srb.ns@hotmail.com>
2016-03-17 23:40:27 +0100

    Initial commit

---
+++ app/src/main/java/rtrk/pnrs/gameclock/MainActivity.java
@@ -0,0 +1,15 @@
+package rtrk.pnrs.gameclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
